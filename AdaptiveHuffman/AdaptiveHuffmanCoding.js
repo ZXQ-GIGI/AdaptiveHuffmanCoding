a20395259ec440cc1b2732c185cac26e979a09d6
gigi <snailzhao1995@163.com>
2017-05-02 09:34:38 +0800

    update

--- AdaptiveHuffman/AdaptiveHuffmanCoding.js
+++ AdaptiveHuffman/AdaptiveHuffmanCoding.js
@@ -26,8 +26,8 @@ function Node(data,left,right){
 }
 
 function BinaryTree(){
-	this.root = new Node(NYT,null,null,null);
-	this.root.num = -1;
+	this.root = new Node('root',null,null,null);
+	//this.root.num = -1;
 	this.warehouse = new Array();
 }
 
@@ -58,42 +58,51 @@ BinaryTree.prototype.levelTraversal = function(node) {
 
 
 BinaryTree.prototype.newToInsert = function(new_data) {
-	console.log(new_data);
-	var current;//node need to add 1
-	var array = this.levelTraversal(this.root);
 	
-	if(this.isExist(new_data)){//the new data has already existed in the tree			
+	var current = new Node(null,null,null);//node need to add 1
+	var array = this.levelTraversal(this.root);
+	//console.log(array);
+	if(this.isExist(new_data)){	
+		//the new data has already existed in the tree	
+		current = new Node(null,null,null);	
+		console.log(new_data + ' is existed.');	
 		for(var i = 0; i < array.length; i++){
 			if(array[i].data == new_data){
 				current = array[i];
-				console('x:'+current);
 			}
 		}	 
 	}
 	else{
-		array[array.length-1].data = null;
+		console.log(new_data + ' is not existed.');	
+		current = new Node(null,null,null);
+		array[array.length-1].data = null;		
 		array[array.length-1].left = new Node(NYT,null,null);
-		array[array.length-1].right = new Node('new_data',null,null);
+		array[array.length-1].right = new Node(new_data,null,null);
 		array[array.length-1].right.count();
 		current = array[array.length-1];
-		console.log('y:'+current);
 	}
-	//console.log(current);
-	while(current.getNum() != this.root.getNum()){
+
+	while(current.data != this.root.data){
 		var currentNum = current.getNum();
 		array = this.levelTraversal(this.root);
-		var farthest;
+		var farthest = new Node(null,null,null);
+
 		for(var i = 0; i < array.length; i++){
 			if(array[i].num == currentNum){
 				farthest = array[i];
 			}
 		}
+	//	console.log(farthest);
 		current = this.updateTree(array,current,farthest);
 	}
+	this.root = current;
+	this.root.data = 'root';
 	this.root.count();
+	console.log(this.root);
 };
 
 BinaryTree.prototype.isExist = function(newdata) {
+
 
 	for(var i = 0; i < this.warehouse.length; i++){
 		if(this.warehouse.indexOf(newdata) >= 0){
@@ -105,6 +114,13 @@ BinaryTree.prototype.isExist = function(newdata) {
 };
 
 BinaryTree.prototype.updateTree = function(array,cur_node,far_node) {
+
+	if(cur_node == far_node){
+		array[1].count();
+		array[0].left = array[2];
+		array[0].right = array[1];
+		return array[0];
+	}
 
 	for(var i = 1; i < 3; i++){
 		if(array[i]){
@@ -123,8 +139,10 @@ BinaryTree.prototype.updateTree = function(array,cur_node,far_node) {
 			}
 		}
 	}
-	array[0].left = array[1];
-	array[0].right = array[2];
+	array[0].left = array[2];
+	array[0].right = array[1];
+	console.log('----------------------------');
+	console.log(array[0]);
 	return array[0];
 }
 
@@ -140,6 +158,7 @@ function AdaptiveHuffman(filePath){
 			for(var i = 0; i < data.length; i++){
 				console.log(i + ":" + data[i]);
 				this.huffmanTree.newToInsert(data[i]);
+				//console.log(this.huffmanTree);
 			}
 		});
 	}
